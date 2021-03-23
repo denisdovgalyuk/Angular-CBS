903fb8e9eaa946e0b6482c76a47c1fbcf39eb069
denisdovgalyuk <denisdovgalyuk@gmail.com>
2021-03-23 10:47:53 +0200

    added HW3 (task2)

--- HW3/directives/src/app/my-table/my-table.component.ts
+++ HW3/directives/src/app/my-table/my-table.component.ts
@@ -8,17 +8,21 @@ import {Component, Input, OnChanges, Output, EventEmitter} from '@angular/core';
 export class MyTableComponent implements OnChanges {
 
   Products = [
-    { id: 1, name : "product 1", price : 100 },
-    { id: 2, name : "product 2", price : 200 },
-    { id: 3, name : "product 3", price : 300 },
-    { id: 4, name : "product 4", price : 400 },
-    { id: 5, name : "product 5", price : 500 },
-    { id: 6, name : "product 6", price : 600 },
-    { id: 7, name : "product 7", price : 700 },
-    { id: 8, name : "product 8", price : 800 },
-    { id: 9, name : "product 9", price : 900 },
-    { id: 10, name : "product 10", price : 1000 }
+    { id: 1, name : "product 1", price : 100, category: 'Category 1' },
+    { id: 2, name : "product 2", price : 200, category: 'Category 1' },
+    { id: 3, name : "product 3", price : 300, category: 'Category 1' },
+    { id: 4, name : "product 4", price : 400, category: 'Category 2' },
+    { id: 5, name : "product 5", price : 500, category: 'Category 2' },
+    { id: 6, name : "product 6", price : 600, category: 'Category 2' },
+    { id: 7, name : "product 7", price : 700, category: 'Category 3' },
+    { id: 8, name : "product 8", price : 800, category: 'Category 3' },
+    { id: 9, name : "product 9", price : 900, category: 'Category 3' },
+    { id: 10, name : "product 10", price : 1000, category: 'Category 3' }
   ];
+
+  productCategory = this.Products;
+
+  category: Array<string> = ['Category 1', 'Category 2', 'Category 3'];
 
   titles: Array<string> = Object.keys(this.Products[0]);
 
@@ -34,8 +38,17 @@ export class MyTableComponent implements OnChanges {
   }
 
   deleteRow(id): void {
+    this.productCategory = this.productCategory.filter(row => id !== row.id);
     this.Products = this.Products.filter(row => id !== row.id);
     this.deleted.emit(id);
+  }
+
+  filterRow(optionName): void {
+    this.productCategory = this.Products;
+    this.productCategory = this.productCategory.filter(row => {
+      return row.category === optionName;
+    })
+
   }
 
 }
